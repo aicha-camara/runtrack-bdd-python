04abb3e7912dd8cbcfd4dedc737a91385a486f8d
aicha-camara <146710153+aicha-camara@users.noreply.github.com>
2024-01-29 16:54:45 +0100

    jour1 runtrack

---
+++ MySQL/jour01/job17.sql
@@ -0,0 +1,6 @@
+mysql> UPDATE etudiant
+SET age = 20
+WHERE nom = 'Betty';
+
+SELECT * FROM etudiant
+WHERE nom = 'Betty';
---
+++ MySQL/jour01/job21.sql
@@ -0,0 +1,2 @@
+SELECT COUNT(*) AS nombre_etudiants_18_25 FROM etudiant
+WHERE age >= 18 AND age <= 25;
